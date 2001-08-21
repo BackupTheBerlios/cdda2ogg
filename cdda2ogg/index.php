0239908acd2e6234e920603eff95beac03abb1be
waldb <waldb>
2001-08-21 19:57:44 +0000

    Initial revision

---
+++ cdda2ogg/index.php
@@ -0,0 +1,41 @@
+<?php
+
+echo '
+
+    <html>
+    <head>
+    <title>cdda2ogg - Homepage</title>
+    </head>
+
+    <body>
+    <p>
+    <b>Beschreibung:</b><br>
+    CDDA2OGG ist ein kleines Perlscript, welches man zum<br>
+    Grabben von Audio-CD\'s und Encoden in Ogg/Vorbis verwenden kann.<br>
+
+    Authors:<br>  <a href="mailto:waldemar.brodkorb@web.de"> 
+                  Waldemar Brodkorb <waldemar.brodkorb@web.de></a><br>
+                  <a href="mailto:GeekuX@web.de">
+                  Richard Höchenberger <GeekuX@web.de></a><br>
+    <br>
+                  Lizens:       GPL 2<br>
+    <br>
+                  Features:     CDDB-Support<br>
+    <br>
+                  Benötigt:<br>
+				<br>
+ 				cdparanoia<br>
+ 				perl<br>
+                                vorbis-tools<br>
+                                libfreedb-perl<br>
+    <br>
+    </p>
+    <center><b><a href="cdda2ogg">Download CDDA2OGG</a></center>
+    <center><b><a href="cdda2ogg-gui">Download the CDDA2OGG-GUI (Perl/Tk)</a></center>
+
+    </body>
+
+    </html> ';
+
+
+?>
